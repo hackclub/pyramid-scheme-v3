78eefdb4a81501fe22df5284ca02949247f098d5
Parth <215315028+nystar1@users.noreply.github.com>
2026-03-08 04:47:54 +0530

    fix: make deploy migrations repeat-safe

--- db/migrate/20260307215036_add_date_of_birth_to_users_and_banner_to_campaigns.rb
+++ db/migrate/20260307215036_add_date_of_birth_to_users_and_banner_to_campaigns.rb
@@ -1,7 +1,13 @@
 class AddDateOfBirthToUsersAndBannerToCampaigns < ActiveRecord::Migration[8.1]
-  def change
-    add_column :users, :date_of_birth, :date
-    add_column :campaigns, :banner_text, :text
-    add_column :campaigns, :banner_type, :string, default: "info"
+  def up
+    add_column :users, :date_of_birth, :date, if_not_exists: true
+    add_column :campaigns, :banner_text, :text, if_not_exists: true
+    add_column :campaigns, :banner_type, :string, default: "info", if_not_exists: true
+  end
+
+  def down
+    remove_column :users, :date_of_birth, if_exists: true
+    remove_column :campaigns, :banner_text, if_exists: true
+    remove_column :campaigns, :banner_type, if_exists: true
   end
 end
